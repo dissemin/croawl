cf9f6099c38a086e630e49878b31571f86f0adbd
Antonin Delpeuch <antonin@delpeuch.eu>
2017-07-23 19:04:09 +0100

    Fix coverage

--- accesspredict/__init__.py
+++ accesspredict/__init__.py
@@ -2,3 +2,13 @@
 from gevent import monkey
 monkey.patch_all(thread=False, select=False)
 
+# for some reason everything breaks down if I do not
+# import requests here… (the test coverage gets messed up).
+# This is probably due to the interaction between gevent's
+# monkey-patching and coverage's black magic)
+import requests
+
+# this is just to trick pyflakes into believing that
+# we are actually doing something with the requests module.
+requests_version = requests.__version__
+requests_version += 'hey you'
